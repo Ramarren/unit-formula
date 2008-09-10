4492fd45304ccdee460b5b02a1dcde88548618e2
Ramarren <ramarren@gmail.com>
2008-09-10 08:42:10 +0200

    Remove :bpm from :use.

--- package.lisp
+++ package.lisp
@@ -16,6 +16,6 @@
 ;;     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-(defpackage unit-formulas (:use :cl :iterate :alexandria :bpm)
+(defpackage unit-formulas (:use :cl :iterate :alexandria)
 	    (:export :reduce-unit :convert-unit :same-unit-p
 		     :dimensionless-p :defformula :define-operators))
