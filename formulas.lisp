8df4fffd1dea83f9752031f45fd465cac1c30617
Ramarren <ramarren@gmail.com>
2008-09-10 09:49:01 +0200

    Change indentation.

--- formulas.lisp
+++ formulas.lisp
@@ -31,14 +31,15 @@
 		   (iter (for op in (ensure-list ops))
 			 (collect `(setf (gethash ',op *operators*) ,kind)))))))
 
-(define-operators (+ - abs) :agree
-		  * :multiply
-		  / :divide
-		  expt :expt
-		  sqrt :sqrt
-		  (sin cos tan sinh cosh tanh) :dimensionless
-		  (asin acos atan asinh acosh atanh) :dimensionless
-		  (log exp) :dimensionless)
+(define-operators
+  (+ - abs) :agree
+  * :multiply
+  / :divide
+  expt :expt
+  sqrt :sqrt
+  (sin cos tan sinh cosh tanh) :dimensionless
+  (asin acos atan asinh acosh atanh) :dimensionless
+  (log exp) :dimensionless)
 
 (defclass variable-with-unit (unit)
   ((name :initarg :name :accessor name-of)
@@ -58,76 +59,76 @@
     (unit formula)
     (number (make-instance 'unit :factor formula))
     (symbol
-     (let ((var (gethash formula env)))
-       (if var
-	   var
-	   (let ((u (gethash (intern (symbol-name formula) (find-package :unit-formulas))
-			     *units*)))
-	     (if u
-		 u
-		 (error "Symbol ~a does name neither variable nor a constant." formula))))))
+       (let ((var (gethash formula env)))
+	 (if var
+	     var
+	     (let ((u (gethash (intern (symbol-name formula) (find-package :unit-formulas))
+			       *units*)))
+	       (if u
+		   u
+		   (error "Symbol ~a does name neither variable nor a constant." formula))))))
     (list
-     (cond ((numberp (car formula))
-	    (reduce-unit formula))
-	   ((gethash (car formula) *operators*)
-	    (cons (car formula) (mapcar (rcurry #'unitify-formula-terminals env) (cdr formula))))
-	   (t (error "Unkown operation ~a." (car formula)))))))
+       (cond ((numberp (car formula))
+	      (reduce-unit formula))
+	     ((gethash (car formula) *operators*)
+	      (cons (car formula) (mapcar (rcurry #'unitify-formula-terminals env) (cdr formula))))
+	     (t (error "Unkown operation ~a." (car formula)))))))
 
 (defun verify-formula (formula)
   "Takes unitified formula and returns it's return unit, if correct, signals error if not."
   (if (atom formula)
       formula
       (let ((args (mapcar #'verify-formula (cdr formula))))
-       (ecase (gethash (car formula) *operators*)
-	 (:agree (if (reduce #'same-unit-p args)
-		     (cadr formula)
-		     (error "Units do not agree in ~a" formula)))
-	 (:multiply
-	  (multiply-units args))
-	 (:divide
-	  (divide-units args))
-	 (:expt
-	  (destructuring-bind (unit power) args
-	   (if (dimensionless-p power)
-	       (expt-units unit (factor-of power))
-	       (error "Power in expt in ~a cannot have an unit" formula))))
-	 (:sqrt
-	  (if (length= args 1)
-	      (sqrt-units (car args))
-	      (error "Cannot sqrt more that one argument in ~a." formula)))
-	 (:dimensionless
-	  (if (every #'dimensionless-p args)
-	      (car args)
-	      (error "Operation ~a needs dimensionless arguments." formula)))))))
+	(ecase (gethash (car formula) *operators*)
+	  (:agree (if (reduce #'same-unit-p args)
+		      (cadr formula)
+		      (error "Units do not agree in ~a" formula)))
+	  (:multiply
+	     (multiply-units args))
+	  (:divide
+	     (divide-units args))
+	  (:expt
+	     (destructuring-bind (unit power) args
+	       (if (dimensionless-p power)
+		   (expt-units unit (factor-of power))
+		   (error "Power in expt in ~a cannot have an unit" formula))))
+	  (:sqrt
+	     (if (length= args 1)
+		 (sqrt-units (car args))
+		 (error "Cannot sqrt more that one argument in ~a." formula)))
+	  (:dimensionless
+	     (if (every #'dimensionless-p args)
+		 (car args)
+		 (error "Operation ~a needs dimensionless arguments." formula)))))))
 
 ;;; formula variables and constant are described by (name unit &optional value)
 (defun make-formula-environment (variable-specs)
   (alist-hash-table
    (iter (for spec in variable-specs)
 	 (destructuring-bind (name unit &optional (value nil)) spec
-	     (let ((unit-instance (reduce-unit unit)))
-	       (collect
-		   (cons name
-			 (make-instance 'variable-with-unit
-					:factor (factor-of unit-instance)
-					:units (units-of unit-instance)
-					:name name
-					:value value))))))))
+	   (let ((unit-instance (reduce-unit unit)))
+	     (collect
+	      (cons name
+		    (make-instance 'variable-with-unit
+				   :factor (factor-of unit-instance)
+				   :units (units-of unit-instance)
+				   :name name
+				   :value value))))))))
 
 (defun replace-formula-terminals (formula)
   "Take unitified environment and transform it into list passable to defun, with all units conversions in place."
   (etypecase formula
     (variable-with-unit
-     (if (value-of formula)
-	 (* (value-of formula) (factor-of formula))
-	 (if (= (factor-of formula) 1)
-	     (name-of formula)
-	     `(* ,(factor-of formula) ,(name-of formula)))))
+       (if (value-of formula)
+	   (* (value-of formula) (factor-of formula))
+	   (if (= (factor-of formula) 1)
+	       (name-of formula)
+	       `(* ,(factor-of formula) ,(name-of formula)))))
     (unit
-     (factor-of formula))
+       (factor-of formula))
     (list
-     (cons (car formula)
-	   (mapcar #'replace-formula-terminals (cdr formula))))))
+       (cons (car formula)
+	     (mapcar #'replace-formula-terminals (cdr formula))))))
 
 ;;; in-spec defines variables and constant with which formula is described defformula creates a
 ;;; function which takes arguments in forms (name value &optional unit), converts them, checks if
@@ -152,15 +153,15 @@
   (iter (for (name unit) in arglist)
 	(for (arg-name arg-value . arg-unit) = (assoc name args))
 	(collect
-	    (etypecase arg-value
-	      (null (error "Missing argument ~a." name))
-	      (unit (if (same-unit-p arg-value unit)
-			(/ (factor-of arg-value) (factor-of unit))
-			(error "Argument ~a has wrong unit, is ~a should be ~a." name arg-value unit)))
-	      (number (let ((arg-unit (reduce-unit arg-unit)))
-			(if (same-unit-p arg-unit unit)
-			    (* arg-value (/ (factor-of arg-unit) (factor-of unit)))
-			    (error "Argument ~a has wrong unit, is ~a should be ~a." name arg-unit unit))))))))
+	 (etypecase arg-value
+	   (null (error "Missing argument ~a." name))
+	   (unit (if (same-unit-p arg-value unit)
+		     (/ (factor-of arg-value) (factor-of unit))
+		     (error "Argument ~a has wrong unit, is ~a should be ~a." name arg-value unit)))
+	   (number (let ((arg-unit (reduce-unit arg-unit)))
+		     (if (same-unit-p arg-unit unit)
+			 (* arg-value (/ (factor-of arg-unit) (factor-of unit)))
+			 (error "Argument ~a has wrong unit, is ~a should be ~a." name arg-unit unit))))))))
 
 (defmacro defformula (name (&rest in-spec) formula-expression)
   (let ((arglist (make-argument-list in-spec))
